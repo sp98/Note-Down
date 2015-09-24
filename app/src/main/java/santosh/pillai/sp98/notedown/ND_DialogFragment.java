d871407712d9b244487a8a7459620c4258ed9c9c
sp98 <santosh.pillai98@gmail.com>
2015-09-24 13:37:14 +0530

    updated the package name

---
+++ app/src/main/java/santosh/pillai/sp98/notedown/ND_DialogFragment.java
@@ -0,0 +1,217 @@
+
+/* Class Name: ND_MainActivity
+ * Version : ND-1.0
+ * Data: 09.19.15
+ * CopyWrit:
+ *
+ */
+
+
+package santosh.pillai.sp98.notedown;
+
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+
+/**
+ * Created by Santosh on 9/19/2015.
+ *
+ * This class creates a Dialog Fragment based on what mode the user has selected.
+ * 1 - Delete Selected Entries Mode
+ * 2.- Delete All Mode
+ * 3.- Exit Mode
+ * 4.- Welcome Dialog Fragment Mode.
+ */
+
+
+public class ND_DialogFragment extends DialogFragment {
+
+    Button byes, bno, dismiss_button, gotIt_button, add_button;
+    TextView dialog_message;
+    Communicator communicator;
+    int myMode;   // This is made equal to the either 1, 2, 3 or 4.
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        Bundle args = getArguments();
+        myMode = args.getInt("MyMode");
+        communicator = (Communicator) activity;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the theme of the Dialog Fragment based on the Mode
+        if((myMode==1)|| (myMode==2) || (myMode==3)){
+
+            setStyle(DialogFragment.STYLE_NORMAL, android.R.style.Theme_Light);
+        }
+        else{
+            setStyle(STYLE_NORMAL, android.R.style.Theme_DeviceDefault_NoActionBar_TranslucentDecor); // dark screen on the whole scree
+
+        }
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // defining the width and height of the dialog fragment if the mode is 1 (Delete), 2(Delete all)  or 3 (Exit)
+        if((myMode==1)|| (myMode==2) || (myMode==3)){
+            int width = 550;
+            int height = 220;
+
+            if(getDialog()!=null){
+                getDialog().getWindow().setLayout(width,  height);
+            }
+        }
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Set the Layout of the Dialog fragment based on the Mode.
+        View v= null;
+        if((myMode==1)|| (myMode==2) || (myMode==3)){
+             v = inflater.inflate(R.layout.nd_dialogfragment_layout, null);
+        }
+       else {
+            v= inflater.inflate(R.layout.nd_welcome_guide_layout, null);
+        }
+
+        setCancelable(false);  //Dialog fragment can not be cancelled by clicking outside.
+
+
+        //Initialization of butons
+        byes = (Button) v.findViewById(R.id.OK);
+        bno = (Button) v.findViewById(R.id.Cancel);
+        dismiss_button= (Button) v.findViewById(R.id.dismiss);
+        gotIt_button = (Button) v.findViewById(R.id.gotIt);
+        add_button = (Button) v.findViewById(R.id.add_button_on_welcome_message);
+
+        dialog_message = (TextView) v.findViewById(R.id.dialog_message);
+
+
+        // Setting the text message and title on the diaglog fragment based on the current Mode.
+        switch(myMode){
+            case 1:
+                getDialog().setTitle("Delete");
+                dialog_message.setText("Delete Selected Notes? ");
+                dialog_message.setTextSize(17);
+                onClickOperations();
+                break;
+
+            case 2:
+
+                getDialog().setTitle("Delete");
+                dialog_message.setText("Delete All Notes?");
+                dialog_message.setTextSize(17);
+                onClickOperations();
+                break;
+
+            case 3:
+
+                getDialog().setTitle("Exit");
+                dialog_message.setText("Exit Note Down? ");
+                dialog_message.setTextSize(17);
+                onClickOperations();
+                break;
+
+            case 4:
+
+                dismiss_button.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        dismiss();
+                        communicator.welcomeMessageState(true);
+                    }
+                });
+
+                gotIt_button.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        dismiss();
+                        communicator.welcomeMessageState(false);
+
+                    }
+                });
+
+                add_button.setOnClickListener(new View.OnClickListener(){
+
+                    @Override
+                    public void onClick(View v) {
+                        dismiss();
+                        Intent i = new Intent(getActivity(), ND_NoteDetailScreen.class);
+                        startActivity(i);
+
+                    }
+                });
+
+                break;
+
+            default:
+        }
+
+        return v;
+
+    }
+
+    // Determines the user action on the Dialog fragments on Mode 1, 2 and 3
+    public void onClickOperations(){
+
+        byes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(myMode==3){
+                    dismiss();
+                    ND_MainActivity ma = new ND_MainActivity();
+                    Intent intent = new Intent(Intent.ACTION_MAIN);
+                    intent.addCategory(Intent.CATEGORY_HOME);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    ma.finish();
+
+                }
+
+                communicator.deleteEntries(true, myMode);
+                dismiss();
+            }
+        });
+
+        bno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(myMode==3){
+                    dismiss();
+                }
+
+                communicator.deleteEntries(false, myMode);
+                dismiss();
+            }
+        });
+
+
+    }
+
+    interface Communicator {
+        public static int  deleteAll = 1;
+        public static int deleteSelected = 2;
+        public void deleteEntries(boolean b, int mode);
+        public void welcomeMessageState (boolean b);
+
+    }
+}
---
+++ app/src/main/java/santosh/pillai/sp98/notedown/ND_SettingsDialogBox.java
@@ -0,0 +1,119 @@
+package santosh.pillai.sp98.notedown;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+
+/**
+ * Created by Santosh on 9/19/2015.
+ */
+public class ND_SettingsDialogBox  extends DialogFragment{
+    Button ascendingOrder;
+    Button descedingOrder;
+    Button byCreatedTime;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, android.R.style.Theme_Light);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        int width = 400;
+        int height = 450;
+
+        if(getDialog()!=null){
+            getDialog().getWindow().setLayout(width, height);
+        }
+    }
+
+    @Override
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.nd_settingsdialogfragment_layout, null);
+        ascendingOrder = (Button) v.findViewById(R.id.ascendingOrder);
+        descedingOrder= (Button) v.findViewById(R.id.descendingOrder);
+        byCreatedTime = (Button) v.findViewById(R.id.byCreatedTime);
+
+
+        getDialog().setTitle("Sort");
+
+        ascendingOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ND_MainActivity.sortOrder=2;
+                dismiss();
+                //ma.retriveAllData();
+                backToMainActivity();
+
+
+            }
+        });
+
+        descedingOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ND_MainActivity.sortOrder=3;
+                dismiss();
+                //ma.retriveAllData();
+                backToMainActivity();
+
+
+            }
+        });
+
+        byCreatedTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ND_MainActivity.sortOrder=1;
+                dismiss();
+                //ma.retriveAllData();
+                backToMainActivity();
+
+            }
+        });
+
+
+
+        return v;
+
+    }
+
+    // Starts the main activity again after refreshing the page.
+    public void backToMainActivity(){
+        Intent i = new Intent(getActivity(),ND_MainActivity.class);
+        startActivity(i);
+
+    }
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // to remove the title space in the dialog fragment
+
+
+        return super.onCreateDialog(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+
+    }
+}
+
